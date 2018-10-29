490c2661c49576b9f2ddc806c05132e8eb16c180
alimac <altkowa@gmail.com>
2018-10-29 04:01:58 +0200

    بعد درس Update

--- app/src/main/java/com/arabcoding/realm/MainActivity.kt
+++ app/src/main/java/com/arabcoding/realm/MainActivity.kt
@@ -17,30 +17,33 @@ class MainActivity : AppCompatActivity() {
                 .name("person.realm").build()
 
         val realm =Realm.getInstance(config)
-        realm.beginTransaction()
-            id = realm.where(Person::class.java).findAll().size
 
+        //create
+        realm.beginTransaction()
+        id = realm.where(Person::class.java).findAll().size
         val person = realm.createObject(Person::class.java,id+1)
         person.name = "علي"
         person.age = 22
         person.job = "مدير"
         realm.commitTransaction()
+        //end create
 
         //update
-        val allPerson = realm.where(Person::class.java).equalTo("name","أحمد").findFirst()
+        val allPerson = realm.where(Person::class.java).equalTo("name",  "أحمد").findFirst()
         realm.beginTransaction()
         allPerson?.job = "سكرتير"
-
-
         /*allPerson.forEach { pers ->
             pers.job = "عامل"
         }*/
         realm.commitTransaction()
+        //end update
 
+        //read
         val all = realm.where(Person::class.java).findAll()
         all.forEach { per ->
             println( "id: " + per.id +  " name: " +per.name + " job: " + per.job)
         }
+        //end read
     }
 
 }
